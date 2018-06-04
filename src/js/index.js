f180a0733b9fd76f316cd1ed8311283978028c6a
zj <1217336430@qq.com>
2018-06-04 10:29:18 +0800

    init

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill';
+import '../static/flexible';
+import '../css/index.scss';
+// import { getQueryValue, play } from '../static/helper';
+// import '../static/vconsole.min';
+
+// const qr = 'https://qiniu.image.cq-wnl.com/content/201805315a71351fb9684f72bfd3448f19cdaf4b.jpg';
+
+
+window.onload = function() {
+  console.log('test');
+};
